30a37587e9d9dfbadac28e2674d8751aa016a4bd
ayush2297 <ayush.saraf47@gmail.com>
2019-11-25 10:21:07 +0530

    added: functionality to check password

--- src/test/java/com/bridgelabz/test/FormValidationTest.java
+++ src/test/java/com/bridgelabz/test/FormValidationTest.java
@@ -15,58 +15,50 @@ public class FormValidationTest
     @Test
     public void givenFirstName_WhenCorrect_ReturnsTrue()
     {
-        boolean name = formValidation.check_Name("Akshay");
-        Assert.assertTrue(name);
+        boolean fname = formValidation.check_Name("Ayush");
+        Assert.assertTrue(fname);
     }
 
     @Test
     public void givenFirstName_WhenPassedSpChar_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("Aksh@y");
-        Assert.assertFalse(name);
+        boolean fname = formValidation.check_Name("A@yush");
+        Assert.assertFalse(fname);
     }
 
     @Test
-    public void givenFirstName_WhenPassedSmallChar_ReturnsFalse()
+    public void givenFirstName_WhenStartsWithSmallChar_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("akshay");
-        Assert.assertFalse(name);
+        boolean fname = formValidation.check_Name("ayush");
+        Assert.assertFalse(fname);
     }
 
     @Test
     public void givenFirstName_WhenPassedNumeric_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("aks123");
-        Assert.assertFalse(name);
+        boolean fname = formValidation.check_Name("ayu123");
+        Assert.assertFalse(fname);
     }
 
     @Test
     public void givenFirstName_WhenPassedMultipleCaps_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("AKSHAY");
-        Assert.assertFalse(name);
+        boolean fname = formValidation.check_Name("AYUSH");
+        Assert.assertFalse(fname);
     }
 
     @Test
-    public void givenFirstName_WhenPassedTwoChar_ReturnsFalse()
+    public void givenFirstName_WhenHasLengthLessThanThree_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("Ak");
-        Assert.assertFalse(name);
-
-    }
-
-    @Test
-    public void givenFirstName_WhenPassedMinAcceptedChar_ReturnsTrue()
-    {
-        boolean name = formValidation.check_Name("Aks");
-        Assert.assertTrue(name);
+        boolean fname = formValidation.check_Name("AY");
+        Assert.assertFalse(fname);
 
     }
 
     @Test
     public void givenLastName_WhenCorrect_ReturnsTrue()
     {
-        boolean lname = formValidation.check_Name("Patwari");
+        boolean lname = formValidation.check_Name("Saraf");
         Assert.assertTrue(lname);
     }
 
@@ -74,74 +66,64 @@ public class FormValidationTest
     @Test
     public void givenLastName_WhenContainsSpChar_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("Patw@ri");
-        Assert.assertFalse(name);
+        boolean lname = formValidation.check_Name("Sar@f");
+        Assert.assertFalse(lname);
     }
 
     @Test
     public void givenLastName_WhenStartsWithSmallChar_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("patwari");
-        Assert.assertFalse(name);
+        boolean lname = formValidation.check_Name("saraf");
+        Assert.assertFalse(lname);
     }
 
     @Test
     public void givenLastName_WhenPassedNumeric_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("patwar1");
-        Assert.assertFalse(name);
+        boolean lname = formValidation.check_Name("Sara7");
+        Assert.assertFalse(lname);
     }
 
     @Test
     public void givenLastName_WhenPassedMultipleCaps_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("PATWARI");
-        Assert.assertFalse(name);
+        boolean lname = formValidation.check_Name("SarAf");
+        Assert.assertFalse(lname);
     }
 
     @Test
-    public void givenLastName_WhenPassedTwoChar_ReturnsFalse()
+    public void givenLastName_WhenHasLengthLessThanThree_ReturnsFalse()
     {
-        boolean name = formValidation.check_Name("Pa");
-        Assert.assertFalse(name);
+        boolean lname = formValidation.check_Name("Sa");
+        Assert.assertFalse(lname);
 
     }
 
-
-    @Test
-    public void givenLastName_WhenPassedMinAcceptedChar_ReturnsTrue()
-    {
-        boolean name = formValidation.check_Name("Aks");
-        Assert.assertTrue(name);
-
-    }
-
-
-    @Test
+	@Test
     public void givenMobileNumber_WhenPassedValid_ReturnTrue()
     {
-        boolean number = formValidation.check_MobileNumber("91 9876543210");
+        boolean number = formValidation.check_MobileNumber("91 8793432373");
         Assert.assertTrue(number);
     }
 
     @Test
     public void givenMobileNumber_WhenNotPassedCountryCode_ReturnFalse()
     {
-        boolean number = formValidation.check_MobileNumber("9876543210");
+        boolean number = formValidation.check_MobileNumber("8793432373");
         Assert.assertFalse(number);
     }
 
     @Test
     public void givenMobileNumber_WhenNotSeperatedCountryCode_ReturnFalse()
     {
-        boolean number = formValidation.check_MobileNumber("919876543210");
+        boolean number = formValidation.check_MobileNumber("918793432373");
         Assert.assertFalse(number);
     }
 
     @Test
     public void givenMobileNumber_WhenPassedWithMultipleSpace_ReturnFalse()
     {
-        boolean number = formValidation.check_MobileNumber("91   9876543210");
+        boolean number = formValidation.check_MobileNumber("91   8793432373");
         Assert.assertFalse(number);
     }
 
@@ -149,14 +131,14 @@ public class FormValidationTest
     @Test
     public void givenMobileNumber_WhenPassedLessThanTenNumbers_ReturnFalse()
     {
-        boolean number = formValidation.check_MobileNumber("91 3210");
+        boolean number = formValidation.check_MobileNumber("91 8793432");
         Assert.assertFalse(number);
     }
 
     @Test
     public void givenMobileNumber_WhenPassedOtherThanNumeric_ReturnFalse()
     {
-        boolean number = formValidation.check_MobileNumber("987ASD54@210");
+        boolean number = formValidation.check_MobileNumber("988793@3f3t3");
         Assert.assertFalse(number);
     }
 
